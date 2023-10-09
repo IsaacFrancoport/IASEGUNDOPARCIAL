b77b8660c0c758ae2a5959741aed38c3ba851eb3
IsaacFrancoport <143574172+IsaacFrancoport@users.noreply.github.com>
2023-10-08 23:50:08 -0600

    5 parcia 2 tarea.py
    En este tipo de algoritmos cada elemento es comparado contra los demás. En la comparación
    se cuenta cuántos elementos son más pequeños que el elemento que se está analizando,
    generando así una “enumeración”. El número generado para cada elemento indicará su
    posición

---
+++ 5 parcial 2 tarea.py
@@ -0,0 +1,4 @@
+mi_lista = ["manzana", "banana", "cereza", "uva", "pera", "sandia", "limon", "coco", "piña", "ciruela", "pepino",]
+
+for indice, elemento in enumerate(mi_lista):
+    print("El elemento en el índice", indice, "es", elemento)
